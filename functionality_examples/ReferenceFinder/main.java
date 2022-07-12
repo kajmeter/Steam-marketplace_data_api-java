64323f0a3f3fb2325c672792dc28daaa58887d63
kajmeter <85412627+kajmeter@users.noreply.github.com>
2022-07-12 13:54:50 +0200

    Updated to meet/show new release changes

--- functionality_examples/ReferenceFinder/main.java
+++ functionality_examples/ReferenceFinder/main.java
@@ -1,21 +1,31 @@
+import java.util.ArrayList;
+
 public class main {
     public static ReferenceFinder finder;
     static String first_item = "awp asiimov";
     static String second_item = "earbuds";
+    static ArrayList<reference> item_references = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         //when needed ,setting custom appID is a possibility
-         finder = new ReferenceFinder(730);
-         print(first_item);
+        finder = new ReferenceFinder(730);
+        item_references.add(
+                finder.find(first_item)
+        );
 
         //you can choose from 6 predefined appIDs
         finder.setAppID(ReferenceFinder.appID.TF2);
-        print(second_item);
+        item_references.add(
+                finder.find(second_item)
+        );
+        print();
     }
 
-    static void print(String item_name) throws Exception {
-        System.out.println("I'm finding first "+item_name+" listing link under "
-                +finder.getAppID()+" app id: \n"
-                +finder.find(item_name)+"\n");
+    static void print() throws Exception {
+        for (reference reference : item_references){
+            System.out.println("I'm finding first "+reference.getSearch_name()+" listing link under "
+                    +reference.getAppID()+" app id: \n"
+                    +reference.getLink()+"\n");
+        }
     }
 }
